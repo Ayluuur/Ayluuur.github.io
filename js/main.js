bbce578749022204ecab73de289ad6ed304eb28b
Kuroj-lb <2458190468@qq.com>
2026-09-07 21:35:50 +0800

    Update portfolio and assets

--- js/main.js
+++ js/main.js
@@ -4,9 +4,14 @@ sitemap:
   exclude: 'yes'
 ---
 function toggleMobileMenu() {
-  $('.navigation-wrapper').toggleClass('visible');
-  $('.btn-mobile-menu__icon').toggleClass('hidden');
-  $('.btn-mobile-close__icon').toggleClass('hidden');
+  var isOpen = !$('.navigation-wrapper').hasClass('visible');
+
+  $('.navigation-wrapper').toggleClass('visible', isOpen);
+  $('.btn-mobile-menu__icon').toggleClass('hidden', isOpen);
+  $('.btn-mobile-close__icon').toggleClass('hidden', !isOpen);
+  $('.btn-mobile-menu')
+    .attr('aria-expanded', isOpen ? 'true' : 'false')
+    .attr('aria-label', isOpen ? 'Close navigation' : 'Open navigation');
 }
 
 function revealContent() {
@@ -38,6 +43,76 @@ function scrollToSection(hash) {
   }, 420);
 }
 
+function initImageLightbox() {
+  var $triggers = $('.image-preview-trigger');
+
+  if (!$triggers.length) {
+    return;
+  }
+
+  var $lightbox = $(
+    '<div class="image-lightbox" aria-hidden="true">' +
+      '<div class="image-lightbox__dialog" role="dialog" aria-modal="true" aria-label="Image preview">' +
+        '<button class="image-lightbox__close" type="button" aria-label="Close image preview">&times;</button>' +
+        '<img class="image-lightbox__image" alt="">' +
+      '</div>' +
+    '</div>'
+  ).appendTo('body');
+  var $image = $lightbox.find('.image-lightbox__image');
+  var $dialog = $lightbox.find('.image-lightbox__dialog');
+  var $close = $lightbox.find('.image-lightbox__close');
+  var lastTrigger = null;
+
+  function closeLightbox() {
+    if (!$lightbox.hasClass('is-open')) {
+      return;
+    }
+
+    $lightbox.removeClass('is-open').attr('aria-hidden', 'true');
+    $('body').removeClass('image-lightbox-open');
+    $image.attr('src', '');
+
+    if (lastTrigger) {
+      $(lastTrigger).trigger('focus');
+      lastTrigger = null;
+    }
+  }
+
+  $triggers.on('click.imageLightbox', function (event) {
+    var $trigger = $(this);
+    var $sourceImage = $trigger.find('img');
+    var title = $trigger.attr('data-preview-title') || $sourceImage.attr('alt') || 'Image preview';
+
+    event.preventDefault();
+    lastTrigger = this;
+    $image.attr({
+      src: $trigger.attr('href'),
+      alt: $sourceImage.attr('alt') || title
+    });
+    $dialog.attr('aria-label', title);
+    $lightbox.addClass('is-open').attr('aria-hidden', 'false');
+    $('body').addClass('image-lightbox-open');
+    $close.trigger('focus');
+  });
+
+  $close.on('click.imageLightbox', function (event) {
+    event.preventDefault();
+    closeLightbox();
+  });
+
+  $lightbox.on('click.imageLightbox', function (event) {
+    if (event.target === this) {
+      closeLightbox();
+    }
+  });
+
+  $(document).on('keydown.imageLightbox', function (event) {
+    if (event.key === 'Escape') {
+      closeLightbox();
+    }
+  });
+}
+
 $(document).ready(function () {
   $('a.panel-button').click(function (event) {
     var hash = this.hash;
@@ -65,10 +140,7 @@ $(document).ready(function () {
       $('.navigation-wrapper').addClass('animated bounceInDown');
     }
     toggleMobileMenu();
-  }).keydown(function (event) {
-    if (event.key === 'Enter' || event.key === ' ') {
-      event.preventDefault();
-      $(this).click();
-    }
   });
+
+  initImageLightbox();
 });
